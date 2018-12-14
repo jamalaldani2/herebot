5bcfe979770a80fd2534f5707789a156dec99f3b
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-14 23:44:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -275,15 +275,5 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
  
  
  
-client.on('message', msg => {
 
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
 client.login(process.env.BOT_TOKEN);
